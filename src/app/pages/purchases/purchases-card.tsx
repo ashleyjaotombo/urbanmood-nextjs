e242fee27ec422c01fe877559ce1652197805efd
ashleyjaotombo <harinambinina.ashley@gmail.com>
2025-07-28 19:54:08 +0200

    Update purchases-card.tsx

--- src/app/pages/purchases/purchases-card.tsx
+++ src/app/pages/purchases/purchases-card.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma"
 import { Order } from "@/typescript/order"
 import { OrderCard } from "../orders/order-card"
+import { OrderItem } from "@/typescript/order-item"
 
 
 type Props = {
@@ -20,8 +21,8 @@ export const OrderWindow = async ({ order }: Props) => {
     return (
         <div className="flex flex-col items-center gap-6 w-screen">
             {[...items]
-                .sort((a, b) => a.orderItemId - b.orderItemId)
-                .map((item) => (
+                .sort((a : OrderItem, b : OrderItem) => a.orderItemId - b.orderItemId)
+                .map((item : OrderItem) => (
                     <OrderCard item={item} key={item.orderItemId} />
                 ))}
             <p className="font-bold text-md">Total : {order?.total?.toFixed(2)} €</p>
