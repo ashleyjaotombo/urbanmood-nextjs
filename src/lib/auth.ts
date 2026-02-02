855f8f9d623c13ad25d5cb1a714ea0613fbebb2e
Jaotombo Ashley HARINAMBININA <harinambinina.ashley@gmail.com>
2026-02-02 10:38:18 +0100

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,10 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
-console.log("🔍 GITHUB_CLIENT_ID:", process.env.GITHUB_CLIENT_ID);
-console.log("🔍 GITHUB_CLIENT_SECRET:", process.env.GITHUB_CLIENT_SECRET);
-console.log("🔍 BETTER_AUTH_URL:", process.env.BETTER_AUTH_URL);
-console.log("🔍 DATABASE_URL:", process.env.DATABASE_URL);
+console.log("GITHUB_CLIENT_ID:", process.env.GITHUB_CLIENT_ID);
+console.log("GITHUB_CLIENT_SECRET:", process.env.GITHUB_CLIENT_SECRET);
+console.log("BETTER_AUTH_URL:", process.env.BETTER_AUTH_URL);
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
